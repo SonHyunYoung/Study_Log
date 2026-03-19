935dbe8f01585a4b0151ef2aeed81b461ca08fa8
Son HyunYoung <son2497474@gmail.com>
2026-03-19 12:51:10 +0900

    파일 수정

--- code/app.js
+++ code/app.js
@@ -3,7 +3,13 @@ const express = require("express"); //express 사용
 const port = 3000; //포트 번호 3000
 const app = express(); //express 객체 생성
 
-//app.use(); //
+app.use(express.static(path.join(__dirname, 'public')));  //정적 파일 선언(css 같은거 ㅇㅇ)
+
+app.use(express.json()); //json 사용한다고 선언
+app.use(express.urlencoded({extended : true})); //body-parser
+
+app.use("/", require("./routes/contactRoutes.js"));
+
 app.get("/", (req, res) => { //test
     res.send("Hello, World");
 });
--- code/maria.js
+++ code/maria.js
@@ -1,11 +1,12 @@
+require('dotenv').config(); 
 const mariadb = require('mariadb'); //mariadb 모듈 사용
 
 const pool = mariadb.createPool({ //연결 pool 생성함
-    host : '127.0.0.1',
-    user : 'root',
-    port : 3307,
-    password : '1234',
-    database : 'carddb',
+    host : process.env.DB_HOST,
+    user : process.env.DB_USER,
+    port : process.env.DB_PORT,
+    password : process.env.DB_PASSWORD,
+    database : process.env.DB_PORT,
     connectionLimit : 4
 });
 
