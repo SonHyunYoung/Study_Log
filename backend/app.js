65edf96c25058a6232dc8d501466acedadd8ef0b
Son HyunYoung <son2497474@gmail.com>
2026-03-23 13:49:21 +0900

    Update : router.js 파일에 주석 추가

--- backend/app.js
+++ backend/app.js
@@ -18,11 +18,8 @@ app.use(express.static(path.join(__dirname, 'public')));  //정적 파일 선언
 app.use(express.json()); //json 사용한다고 선언
 app.use(express.urlencoded({extended : true})); //body-parser
 
-app.use("/", require("./middleware/route.js"));
+app.use("/", require("./middleware/router.js"));
 
-app.get("/", (req, res) => { //test
-    res.send("Hello, World");
-});
 
 app.listen(port, () => { //서버 띄우기, 3000번 포트
     console.log(`${port}번 포트에서 실행 중`);
--- backend/middleware/router.js
+++ backend/middleware/router.js
@@ -1,25 +1,23 @@
 const express = require("express");
 
-const router = express.router(); //router 객체 생성
-const pool = require("./maria"); //db 연결 풀
+const router = express.Router(); //router 객체 생성
+const pool = require("../maria"); //db 연결 풀
 
-//로그인
-router
-.get("/", (res, req) => {
-
+router.get("/", (req, res) => {
+    res.send("서버 열렸음 ㅇㅇ");
+    res.status(200);
 });
 
+//로그인
+
 //회원가입 
-router
-.post();
 
 //게시물 crud
-router
-.get();
 
 //오답노트 crud
-router
 
 //메인화면 
 
 //로그아웃
+
+module.exports = router;
