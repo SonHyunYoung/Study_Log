9c3f99c0e4b774a3f6919e12e6ffd833a24629a5
Son HyunYoung <son2497474@gmail.com>
2026-03-23 11:52:33 +0900

    update : app.js 파일 수정

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,18 @@
+const path = require("path"); //path 모듈 사용
+const cors = require("cors"); //cors 사용 cors : react와 통신하기 위한 모듈
 const express = require("express"); //express 사용
+require("dotenv").config(); //env 파일
 
 const port = 3000; //포트 번호 3000
 const app = express(); //express 객체 생성
 
+//cors 사용 설정
+app.use(cors({
+    origin : "http://localhost:5173",
+    credentials : true
+}));
+
+//서버  설정
 app.use(express.static(path.join(__dirname, 'public')));  //정적 파일 선언(css 같은거 ㅇㅇ)
 
 app.use(express.json()); //json 사용한다고 선언
--- backend/middleware/router.js
+++ backend/middleware/router.js
@@ -1,5 +1,25 @@
 const express = require("express");
 
 const router = express.router(); //router 객체 생성
-
 const pool = require("./maria"); //db 연결 풀
+
+//로그인
+router
+.get("/", (res, req) => {
+
+});
+
+//회원가입 
+router
+.post();
+
+//게시물 crud
+router
+.get();
+
+//오답노트 crud
+router
+
+//메인화면 
+
+//로그아웃
